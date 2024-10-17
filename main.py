d21d8bd795fcc18f4618086cc1ae15c2ee13f81b
Mateus Paiva <mateusopaiva@gmail.com>
2024-10-17 14:13:18 -0300

    Escrevendo e lendo a tabela

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pathlib import Path
-from sqlalchemy import create_engine, String, Boolean
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import create_engine, String, Boolean, select
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session
 
 pasta_atual = Path(__file__).parent
 PATH_TO_BD = pasta_atual / 'bd_usuarios.sqlite'
@@ -22,3 +22,49 @@ class Usuario(Base):
   
 engine = create_engine(f'sqlite:///{PATH_TO_BD}')
 Base.metadata.create_all(bind=engine)
+
+# CRUD
+def cria_usuarios(
+    nome,
+    senha,
+    email,
+    **kwargs
+):
+  with Session(bind=engine) as session:
+    usuario = Usuario(
+      nome = nome,
+      senha = senha,
+      email = email,
+      **kwargs
+    )
+    session.add(usuario)
+    session.commit()
+
+def le_todos_usuarios():
+  with Session(bind=engine) as session:
+    comando_sql = select(Usuario)
+    usuarios = session.execute(comando_sql).fetchall()
+    usuarios = [user[0] for user in usuarios]
+    return usuarios
+
+def le_usuario_por_id(id):
+    with Session(bind=engine) as session:
+        comando_sql = select(Usuario).filter_by(id=id)
+        usuarios = session.execute(comando_sql).fetchall()
+        return usuarios[0][0]
+
+if __name__ == '__main__':
+  #cria_usuarios(
+  #  nome = 'Mateus Paiva',
+  #  senha = 'minha_senha',
+  #  email = 'mateus@gmail.com',
+  #)
+
+  #usuarios = le_todos_usuarios()
+  #usuario_0 = usuarios[0]
+  #print(usuario_0)
+  #print(usuario_0.nome, usuario_0.senha, usuario_0.email)
+
+  usuario_mateus = le_usuario_por_id(id=1)
+  print(usuario_mateus)
+  print(usuario_mateus.nome, usuario_mateus.senha, usuario_mateus.email)
